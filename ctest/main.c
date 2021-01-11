6f1e5f7402b6cb3375b6b824380795c4456c4df6
Yujeonja <hardboiled65@gmail.com>
2021-01-11 11:58:05 +0900

    C test added.

--- ctest/main.c
+++ ctest/main.c
@@ -1,6 +1,30 @@
+#include <assert.h>
+#include <stddef.h>
+
 #include <seshat.h>
+
+void test_string_len()
+{
+    sh_string *string = sh_string_new("Hello");
+    size_t len = sh_string_len(string);
+    assert(len == 5);
+
+    sh_string_free(string);
+}
+
+void test_string_print()
+{
+    sh_string *string = sh_string_new("Foo bar");
+    sh_string_print(string);
+
+    sh_string_free(string);
+}
 
 int main()
 {
+    test_string_len();
+
+    test_string_print();
+
     return 0;
 }
--- include/seshat/string.h
+++ include/seshat/string.h
@@ -10,11 +10,17 @@
 #ifndef _SESHAT_STRING_H
 #define _SESHAT_STRING_H
 
+#include <stddef.h>
+
 typedef struct sh_string {
     void *content;
 } sh_string;
 
 sh_string* sh_string_new(const char*);
+
+size_t sh_string_len(sh_string *string);
+
+void sh_string_free(sh_string *string);
 
 void sh_string_print(const sh_string *string);
 
