87af4f3e67e6e4a50d1421166489948be8ba99f8
Yujeonja <hardboiled65@gmail.com>
2021-02-14 04:49:33 +0900

    Correct SONAME.

--- build.rs
+++ build.rs
@@ -1,4 +1,7 @@
 fn main() {
+    // Version info.
+    // Major version.
+    let major_version = env!("CARGO_PKG_VERSION").split('.').next().unwrap();
     println!("cargo:rustc-cdylib-link-arg=-Wl,-soname,libseshat4c.so.{}",
-        env!("CARGO_PKG_VERSION"));
+        major_version);
 }
